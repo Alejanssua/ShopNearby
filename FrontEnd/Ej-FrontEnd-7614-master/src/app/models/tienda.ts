bbd4c9e2d8d413c704727ef5fe7b6eeb6ac2097a
Alejandro Salazar <jossua.salazar@gmail.com>
2020-08-04 01:02:07 -0500

    Shop Nearby V1

---
+++ FrontEnd/Ej-FrontEnd-7614-master/src/app/models/tienda.ts
@@ -0,0 +1,9 @@
+export class Tienda {
+    idTienda:number;
+    Nombre :string;
+    R_Social :string;    
+    N_Telefono :number;
+    Direccion :string;
+    Estado :string;
+    Horario :string;
+}
